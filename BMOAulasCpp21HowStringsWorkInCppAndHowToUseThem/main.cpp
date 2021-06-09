63df30084302c4f82a1b6526add1676c745da7d2
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2021-06-08 22:36:27 -0300

    Initial Commit

---
+++ BMOAulasCpp21HowStringsWorkInCppAndHowToUseThem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BMOAulasCpp21HowStringsWorkInCppAndHowToUseThem
+//
+//  Created by Breno Medeiros on 08/06/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
